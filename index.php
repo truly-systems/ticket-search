08a73aab7c91017bbe8a24d4076f0c90905c9ff5
Augusto Ferronato <augusto.ferronato@gmail.com>
2018-02-06 13:31:00 -0200

    update headers

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
  https://github.com/truly-systems/ticket-search
  -------------------------------------------------------------------------
  LICENSE
- This file is part of TelegramBot.
- TelegramBot is free software; you can redistribute it and/or modify
+ This file is part of Ticket-Search.
+ Ticket-Search is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
- TelegramBot is distributed in the hope that it will be useful,
+ Ticket-Search is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
