d6b38044a9c0cde60eff932f4a977c1f0f376766
Augusto Ferronato <augusto.ferronato@gmail.com>
2018-02-06 13:27:24 -0200

    add copyright

--- src/App/Views/Ticket.php
+++ src/App/Views/Ticket.php
@@ -1,4 +1,24 @@
 <?php
+/*
+ -------------------------------------------------------------------------
+ Ticket-Search  for GLPI
+ Copyright (C) 2018 by the Truly Systems Development Team.
+ https://github.com/truly-systems/ticket-search
+ -------------------------------------------------------------------------
+ LICENSE
+ This file is part of TelegramBot.
+ TelegramBot is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ TelegramBot is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with Ticket-Search. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
 namespace App\Views;
 
 class Ticket {
